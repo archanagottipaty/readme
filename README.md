964e08b875c02b5080c5bd8cdb6423a0fc8ea191
Archana Gottipaty <archana.gottipaty@gmail.com>
2020-12-03 16:46:38 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# calculator
+
+## Table of contents
+* [Use Case](#use-case)
+* [Application Objective](#application-objective)
+* [Functionality](#functionality)
+* [Setup](#setup)
+* [Technologies Used](#technologies-used)
+* [Developers](#developers)
---
+++ index.js
@@ -0,0 +1,65 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
+const promptUser = () =>
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Name of your Project Title:'
+      },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please enter the description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter steps required to install: ',
+      default: 'npm install'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter instructions for usage:' ,
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Enter credits:',
+      },
+      {
+        type: 'list',
+        choices: ['mit', 'apache', 'apple'],
+        name: 'license',
+        message: 'Enter License:',
+      },
+
+  ])
+  .then((data) => {
+
+    console.log(data);
+    console.log(generateMD(data));
+   
+
+    fs.writeFile("README.md", generateMD(data), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+
+  promptUser();
+
+const generateMD = (answers) =>
+`# ${answers.title}
+
+![WeatherDash](https://img.shields.io/github/languages/top/archanagottipaty/WeatherDash)
+
+## Table of contents
+* [Use Case](#use-case)
+* [Application Objective](#application-objective)
+* [Functionality](#functionality)
+* [Setup](#setup)
+* [Technologies Used](#technologies-used)
+* [Developers](#developers)`
